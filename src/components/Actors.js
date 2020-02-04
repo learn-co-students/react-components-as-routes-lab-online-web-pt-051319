0744459a5d3b9a7e36b518378545cee5004fea9e
jamievullo <jvullo@protonmail.com>
2020-02-04 12:05:01 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+      <h1>Actors Page</h1>
+        {actors.map((actor, index) => {
+          return (
+            <div className="actor" key={index}>
+              <h2>Name: {actor.name}</h2>
+              <h3>Movies:
+                <ul>
+                  {actor.movies.map((movie, key) => {
+                    return (
+                    <li key={key}>{movie}</li>
+                      )
+                    })
+                  }
+                </ul>
+              </h3>
+            </div>
+            )
+          })
+        }
+      </div>
+    )
+}
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link ={
+  width: "200px",
+  padding: "10px",
+  margin: "10px 6px 6px",
+  background: "green",
+  textDecoration: "none",
+  color: "white",
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink 
+        to="/" 
+        exact
+        style={link}
+        activeStyle={{
+            background: 'darkblue'
+        }}
+      >Home</NavLink>
+      <NavLink 
+          to="/actors" 
+          exact
+          style={link}
+          activeStyle={{
+              background: 'darkblue'
+          }}
+      >Actors</NavLink>
+      <NavLink
+          to="/directors"
+          exact
+          style={link}
+          activeStyle={{
+              background: "darkblue"
+          }}
+      >Directors</NavLink>
+      <NavLink
+          to="/movies"
+          exact
+          style={link}
+          activeStyle={{
+              background: "darkblue"
+          }}
+      >Movies</NavLink>
     </div>
   );
 };
