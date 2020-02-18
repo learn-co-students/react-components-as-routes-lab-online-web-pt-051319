88aca5a60be689f61e89a724d11ed7ff2162d1eb
Jason McGloin <jason.mcgloin@gmail.com>
2020-02-18 00:58:49 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+	const movieMap = movies.map(movie => {
+		return (
+			<div>
+				<h2>{ movie.title }</h2>
+				<p>Runtime: {movie.time}</p>
+				<ul>
+					{ movie.genres.map(movie => <li>{ movie })</li>) }
+				</ul>
+				<p>Score: {movie.metascore}</p>
+			</div>
+		)
+	})
   return (
     <div>
-        {/*{code here}*/}
+    	<h1>Movies Page</h1>
+    	{ movieMap }
     </div>
   );
 };
