cf979af680a10c9a26b9d27422cf78e6c5b9eb46
Michael Mottola <mottola64@gmail.com>
2020-02-27 22:02:47 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,25 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+        {directors.map((director, index) => {
+          return (
+            <div className="director" key={index}>
+              <h2>Name: {director.name}</h2>
+              <h3>Movies:
+                <ul>
+                  {director.movies.map((movie, key) => {
+                    return (
+                    <li key={key}>{movie}</li>
+                      )
+                    })
+                  }
+                </ul>
+              </h3>
+            </div>
+            )
+          })
+        }
     </div>
   );
 }
